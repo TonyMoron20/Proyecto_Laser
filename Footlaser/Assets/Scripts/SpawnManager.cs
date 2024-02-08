6b81a619606120f8806c4ccbc9ef7419b2c363a1
Álvaro Antonio Morón <tonymoron20@gmail.com>
2024-02-08 13:25:36 -0600

    Flying Enemies
    Se agrego el spawn aleatorio de los enemigos voladores

--- Footlaser/Assets/Scripts/SpawnManager.cs
+++ Footlaser/Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject flyingEnemyPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +18,13 @@ public class SpawnManager : MonoBehaviour
         while (true)
         {
             Instantiate(enemyPrefab, new Vector3(10, -3, 0), Quaternion.identity);
+            Instantiate(flyingEnemyPrefab, new Vector3(10f, RandomHeight(), 0f), Quaternion.identity);
             yield return new WaitForSeconds(2.0f);
         }
+    }
+
+    private float RandomHeight()
+    {
+        return Random.Range(0, 4);
     }
 }
